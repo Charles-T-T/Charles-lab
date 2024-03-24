609e7a71688fbcad9d72b9d3fb737e1d27471b37
charles <1592276395@qq.com>
2024-03-24 17:35:20 +0800

    lab2 finished, encoded by GB 2312 for Chinese output

--- program_design_II/labs/lab2/lab2.h
+++ program_design_II/labs/lab2/lab2.h
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <stdio.h>
+#include <cstdlib>
 #include <fstream>
 #include <sstream>
 #include <unordered_map>
@@ -7,24 +8,35 @@
 #include <queue>
 #include <stack>
 #include <vector>
+#include <sstream>
+
+#define DEBUG_MOOD 0
 
 using namespace std;
 
-class FileProc{
+class FileProc
+{
 public:
-    FileProc() {}
+    FileProc() : readPath("nullPath"), writePath("nullPath") {}
     FileProc(const string &readPath, const string &writePath) : readPath(readPath), writePath(writePath) {}
-    ~FileProc() {
+    ~FileProc()
+    {
         if (readFile.is_open())
             readFile.close();
         if (writeFile.is_open())
             writeFile.close();
     }
 
-    int MenuChoose(); // 用户选择菜单上的功能
-    void OpenFile(); // 打开文件
-    void WordsCount(); // 统计词频
+    int MenuChoose();   // 用户选择菜单上的功能
+    void OpenFile();    // 打开文件
+    void RestartRead(); // 重新从头处理readFile
+
+    /* 功能1 */
+    void WordsCount();  // 统计词频
     void WriteResult(); // 写入结果
+
+    /* 功能2 */
+    void LinesWithT(); // 显示带查询词所在的行
 
 private:
     ifstream readFile;                        // 待处理的文件
@@ -34,42 +46,50 @@ private:
     vector<string> topVec;                    // 用一个vector存储结果，便于按顺序展示
 };
 
-int FileProc::MenuChoose(){
+int LegalChoice(string choice, int low, int high); // 检查输入选项是否合法
+int FileProc::MenuChoose()
+{
     cout << "----------------------------" << endl;
     cout << "           主菜单           " << endl;
     cout << "----------------------------" << endl;
-    cout << "1. 统计词频: 出现最多的前50词" << endl;
+    cout << "1. 打印出现最多的前50词" << endl;
     cout << "2. 显示待查询词所在行信息" << endl;
+    cout << "0. 退出程序" << endl;
 
-    int choice;
+    string choice;
     cout << "----------------------------" << endl;
-    cout << "请输入要进行的操作(1-2): ";
+    cout << "请选择要进行的操作(0-2): ";
     cin >> choice;
-    return choice;
+
+    return LegalChoice(choice, 0, 2);
 }
 
 void FileProc::OpenFile()
 {
-    readFile.open(readPath);
-    if (readFile.is_open())
-        cout << readPath << " opened successfully!" << endl;
-    else
+    if (readPath == "nullPath")
     {
-        while (1)
-        {
-            cout << "Fail to open the file, please input the correct path to the file again: ";
-            cin >> readPath;
-            readFile.open(readPath);
-            if (readFile.is_open())
-            {
-                cout << "File opened successfully!" << endl;
-                return;
-            }
-        }
+        cout << "请输入待打开文件的路径：";
+        cin >> readPath;
     }
+    readFile.open(readPath);
+    while (!readFile.is_open())
+    {
+        cout << "文件打开失败，请重新输入文件路径: ";
+        cin >> readPath;
+        readFile.open(readPath);
+    }
+    cout << readPath << "打开成功!" << endl;
 }
 
-void FileProc::WordsCount(){
+void FileProc::RestartRead()
+{
+    readFile.clear();
+    readFile.seekg(0);
+}
+
+void FileProc::WordsCount()
+{
+    this->RestartRead();
     string line, word;
     while (getline(readFile, line))
     {
@@ -95,18 +115,82 @@ void FileProc::WordsCount(){
     }
 
     cout << "-----------------------" << endl;
-    cout << "TOP50 Frequent Strings" << endl;
+    cout << "     TOP50 高频词" << endl;
     cout << "-----------------------" << endl;
     for (int i = topVec.size() - 1; i >= 0; i--)
-        cout << topVec[i] << ": " << wordsCountMap[topVec[i]] << endl;
+        cout << topVec[i] << ":\t\t" << wordsCountMap[topVec[i]] << endl;
     cout << "-----------------------" << endl;
 }
 
 void FileProc::WriteResult()
 {
+    if (writePath == "nullPath")
+    {
+        cout << "请输入待写入文件的路径：";
+        cin >> writePath;
+    }
     writeFile.open(writePath);
-    cout << "Writing the result to " << writePath << "..." << endl;
+    while (!writeFile.is_open())
+    {
+        cout << "文件打开失败，请重新输入文件路径：";
+        cin >> writePath;
+    }
+    cout << "结果正在写入" << writePath << "..." << endl;
     for (int i = topVec.size() - 1; i >= 0; i--)
         writeFile << topVec[i] << ": " << wordsCountMap[topVec[i]] << "\n";
-    cout << "Result written successfully!" << endl;
+    cout << "结果写入成功!" << endl;
+}
+
+void FileProc::LinesWithT()
+{
+    this->RestartRead();
+    string target, line, word;
+    cout << "请输入待查询词：";
+    cin >> target;
+    // TODO debug
+    if (DEBUG_MOOD && !readFile.is_open())
+        cout << "file open failed!" << endl;
+    while (getline(readFile, line))
+    {
+        stringstream ss(line);
+        while (ss >> word)
+        {
+            if (word == target)
+            {
+                cout << line << endl;
+                break;
+            }
+        }
+    }
+    cout << "打印完毕，是否继续查询（1——是，0——否）：";
+    string choice;
+    cin >> choice;
+    int choiceNum = LegalChoice(choice, 0, 1);
+    if (choiceNum)
+        return FileProc::LinesWithT();
+    else
+        return;
+}
+
+// 检测输入值是否为整数
+bool IsInt(string input)
+{
+    bool res = true;
+    for (char const &i : input)
+        if (!isdigit(i))
+        {
+            res = false;
+            break;
+        }
+    return res;
+}
+
+int LegalChoice(string choice, int low, int high)
+{
+    while (!IsInt(choice) || stoi(choice) < low || stoi(choice) > high)
+    {
+        printf("输入有误，请输入一个%d到%d之间的整数：", low, high);
+        cin >> choice;
+    }
+    return stoi(choice);
 }
